d702b6bc788b03da3ec36fb332a6fb8f8f2cc02e
Vijay Anand <va776683@gmail.com>
2025-02-01 12:32:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: [
     "./index.html",
@@ -9,6 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+   daisyui,
   ],
 }
